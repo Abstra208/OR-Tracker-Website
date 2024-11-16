19aed0634bd31854aa6bd2b02a12aba8c49074c5
Ludovic Morin <LudovicMorin357@hotmail.com>
2024-11-15 23:33:56 -0500

    Correction du chemin d'accès à l'icône dans layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="description" content="OR Tracker is a bot designed specifically for the community of the OR server, facilitating the seamless tracking of current records." />
-        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
