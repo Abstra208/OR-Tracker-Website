6e92d59d84a68d1acb7e3bb5ca85dfca6e0d00ea
Ludovic Morin <LudovicMorin357@hotmail.com>
2024-12-08 21:50:48 -0500

    Refactor Records component: add handleSubmit function for navigation; remove unused Users page.

--- src/app/components/records.tsx
+++ src/app/components/records.tsx
@@ -1,9 +1,15 @@
 export default function Records() {
+
+    const handleSubmit = () => {
+        const id = document.getElementById('id') as HTMLInputElement
+        const value = id.value;
+        window.location.href = `/records/${value}`;
+    }
     return (
         <main>
             <h1>Records</h1>
             <input type="text" name="" id="id" />
-            <button>Submit</button>
+            <button onClick={handleSubmit}>Submit</button>
         </main>
     );
 }
